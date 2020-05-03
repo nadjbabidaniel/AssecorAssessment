d5e9c4ac36562d7d84a704cdf93020f209aa31c7
danielnadjbabi <danielnadjbabi@gmail.com>
2020-05-03 20:33:37 +0200

    Private static fields in <Unit Tests>

--- AssecorAssessmentUnitTestProject/PersonApiTest.cs
+++ AssecorAssessmentUnitTestProject/PersonApiTest.cs
@@ -11,33 +11,63 @@ namespace AssecorAssessmentUnitTestProject
     [TestClass]
     public class PersonApiTest
     {
+        private static readonly PersonModel Person = new PersonModel() { Id = 1, Lastname = "Petersen", FirstName = "Peter", City = "Stralsund", Zipcode = "18439", Color = "blau"};
+        private static readonly List<PersonModel> Persons = new List<PersonModel>() { Person };
+
+        private Mock<IParserService> ParserService = new Mock<IParserService>();
+
+        public PersonApiTest()
+        {
+            ParserService.Setup(p => p.ReadCsvFileToEmployeeModel()).Returns(Persons);
+        }
+
+        private static readonly Dictionary<int, string> Dictionary = new Dictionary<int, string>()
+        {
+            { 1, "blau"}, { 2, "grün"}, { 3, "violett"},{ 4, "rot"}, { 5, "gelb"}, { 6, "türkis"}, { 7, "weiß"}
+        };
+
         [TestMethod]
         public void GetPersonsTest()
         {            
-            PersonModel person = new PersonModel()
-            {
-                Id = 1,
-                Lastname = "Petersen",
-                FirstName = "Peter",
-                City = "Stralsund",
-                Zipcode = "18439",                
-                Color = "blau"
-            };
-
-            List<PersonModel> persons = new List<PersonModel>() { person };
-
-            // Arrange
-            var parserService = new Mock<IParserService>();
-            parserService.Setup(p => p.ReadCsvFileToEmployeeModel()).Returns(persons);            
-            
-            var controller = new PersonsController(parserService.Object);
+             // Arrange                               
+            var controller = new PersonsController(ParserService.Object);
 
             // Act
             var jsonResponse = controller.GetPersons();
-            List<PersonModel>  response = JsonConvert.DeserializeObject<List<PersonModel>>(jsonResponse);
+            List<PersonModel> response = JsonConvert.DeserializeObject<List<PersonModel>>(jsonResponse);
 
             // Assert
             Assert.AreEqual(response.Count, 1);
+        }
+
+        [TestMethod]
+        public void GetPersonsIdTest()
+        {
+            // Arrange          
+            var controller = new PersonsController(ParserService.Object);
+
+            // Act
+            var jsonResponse = controller.GetPerson(Person.Id);
+            List<PersonModel> response = JsonConvert.DeserializeObject<List<PersonModel>>(jsonResponse);
+
+            // Assert
+            Assert.AreEqual(response.Count, 1);
+            Assert.AreEqual(response[0].Color, Dictionary[Person.Id]);
+        }
+
+        [TestMethod]
+        public void GetColorTest()
+        {
+            // Arrange           
+            var controller = new PersonsController(ParserService.Object);
+
+            // Act
+            var jsonResponse = controller.GetColor(Person.Color);
+            List<PersonModel> response = JsonConvert.DeserializeObject<List<PersonModel>>(jsonResponse);
+
+            // Assert
+            Assert.AreEqual(response.Count, 1);
+            Assert.AreEqual(response[0].Color, Dictionary[Person.Id]);
         }
     }
 }
